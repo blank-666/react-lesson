f2f886fb9318c47b4ef0b45d976109e33473d716
Nazar <nazshved@gmail.com>
2020-09-16 18:09:11 +0300

    Updates

---
+++ precache-manifest.56e766da4518748ae4fd8c38cbc429e4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb31749837fcfa813e5a6914f2c3883",
+    "url": "/react-lesson/index.html"
+  },
+  {
+    "revision": "fea09e0895e2aec9f953",
+    "url": "/react-lesson/static/css/main.63760881.chunk.css"
+  },
+  {
+    "revision": "6447f0abb7aaa71ec058",
+    "url": "/react-lesson/static/js/2.97ddbbdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-lesson/static/js/2.97ddbbdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fea09e0895e2aec9f953",
+    "url": "/react-lesson/static/js/main.62f2bbe8.chunk.js"
+  },
+  {
+    "revision": "2419dd834acdaab51bad",
+    "url": "/react-lesson/static/js/runtime-main.2b4f04aa.js"
+  },
+  {
+    "revision": "ab5dbefcfa844806551e495c511f2399",
+    "url": "/react-lesson/static/media/comment.ab5dbefc.svg"
+  },
+  {
+    "revision": "918b128f84b467931e6b882d9cbafccd",
+    "url": "/react-lesson/static/media/download.918b128f.svg"
+  },
+  {
+    "revision": "4a8048e5d734107669920e6016c70dc0",
+    "url": "/react-lesson/static/media/like.4a8048e5.svg"
+  },
+  {
+    "revision": "3facbb8ded523d42153f6ac301432c7e",
+    "url": "/react-lesson/static/media/retweet.3facbb8d.svg"
+  },
+  {
+    "revision": "8973fefb9a88895da4004ef6569bf0b6",
+    "url": "/react-lesson/static/media/verified.8973fefb.svg"
+  }
+]);
